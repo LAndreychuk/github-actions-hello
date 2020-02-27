e7bb4fa72def84633c18ecbb5dbc2189e1185787
LAndreychuk <luda.and@gmail.com>
2020-02-27 14:57:01 -0500

    new change

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-console.log('...This is a test file intended to be pushed to github repo so as to trigger my action to run.');
+console.log('......This is a test file intended to be pushed to github repo so as to trigger my action to run.');
